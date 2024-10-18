bf1232d5f504d1a9f449f0c9ce5b23f65a56e58a
Thais Silva <141892213+Thais-Silva27@users.noreply.github.com>
2024-10-18 19:51:05 -0300

    Delete src directory

--- src/components/Product/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-import { cores } from '../../styles'
-
-export const Card = styled.div`
-  background-color: ${cores.branco};
-  padding-top: 50px;
-  margin-left: 171px;
-`
--- src/components/Rodape/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-import { cores } from '../../styles'
-
-export const RodaPe = styled.footer`
-  background-color: ${cores.branco};
-  margin-top: 132px;
-`
